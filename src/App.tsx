fb8758ae55a247089648c4027f196b8e10a6e3dc
mumblejinx <mumblejinx@gmail.com>
2026-04-09 10:59:15 +0900

    Adjust intro background position in App component

--- src/App.tsx
+++ src/App.tsx
@@ -83,7 +83,7 @@ export default function App() {
       <div className="flex-grow flex flex-col relative">
         {/* Layer 1: Intro Background (Static, always at the bottom) */}
         <div className="absolute inset-0 bg-white z-10 flex justify-center p-4">
-          <div className="relative top-[45%] -translate-y-1/2 h-fit">
+          <div className="relative top-[40%] -translate-y-1/2 h-fit">
             <div className="hidden md:block">
               <AssetImage src="/computer_intro.jpg" fallback="[INTRO IMAGE]" className="max-w-full max-h-[70vh]" />
             </div>
